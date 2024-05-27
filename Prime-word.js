b9c150d7e535a5c4e97499e7d31506a390aef749
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-05-26 22:26:43 -0700

    Update Prime-word.js

--- Prime-word.js
+++ Prime-word.js
@@ -18,3 +18,19 @@ Example:
 
 
 // Solution
+
+const isPrimeNumber = num => {
+
+  for (let i = 2; i * i <= num; i++)
+    if (num % i === 0)
+      return false;
+
+  return true;
+
+};
+
+const isPrimeWord = (word, add = 0) => 
+  [...word].some(char => isPrimeNumber(char.charCodeAt(0) + add));
+
+const primeWord = arr =>
+  arr.map(([ name, add ]) => +isPrimeWord(name, add));
